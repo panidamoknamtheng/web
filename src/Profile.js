965020117e85718c880eef908af200db0fed98c9
Dusita Sungklinhom <dusitasungklinhom@gmail.com>
2023-04-10 04:46:12 +0700

    profile mook

--- src/Profile.js
+++ src/Profile.js
@@ -1,100 +1,377 @@
-import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, Alert, Image } from 'react-native'
-import * as ImagePicker from 'expo-image-picker';
-import React, { useState,  } from 'react'
-
+import React, { useState, useEffect, useContext, useCallback } from "react";
+import {
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  Pressable,
+  ScrollView,
+} from "react-native";
+import { validateEmail } from "./utils.js";
+import { AuthContext } from "../contexts/AuthContext";
+import Checkbox from "expo-checkbox";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as ImagePicker from "expo-image-picker";
+import * as SplashScreen from "expo-splash-screen";
 
 const Profile = () => {
-    const [image, setImage] = useState(null);
-    const [uploading, setUploading] = useState(false);
+  const [profile, setProfile] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    image: "",
+  });
+  const [discard, setDiscard] = useState(false);
 
-    const pickImage = async () => {
-        // No permissions request is necessary for launching the image library
-        let result = await ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.All,
-          allowsEditing: true,
-          aspect: [4, 3],
-          quality: 1,
-        });
-    
-        const source = { uri: result.uri };
-        console.log(source);
-        setImage(source);
-
-    };
-
-    const uploadImage = async () => {
-      setUploading(true);
-      const response = await fetch(image.uri)
-      const blob = await response.blob();
-      const filename = image.uri.substring(image.uri.lastIndexOf('/') + 1);
-      var ref = firebase.storage().ref().child(filename).put(blob);
-
+  useEffect(() => {
+    (async () => {
       try {
-        await ref;
+        const getProfile = await AsyncStorage.getItem("profile");
+        setProfile(JSON.parse(getProfile));
+        setDiscard(false);
       } catch (e) {
         console.error(e);
       }
-      setUploading(false);
-      Alert.alert(
-        'Photo uploaded!!!',
-      );
-      setImage(null);
-    };
+    })();
+  }, [discard]);
+
+  const validateName = name => {
+    if (name.length > 0) {
+      return name.match(/[^a-zA-Z]/);
+    } else {
+      return true;
+    }
+  };
+
+  const validateNumber = number => {
+    if (isNaN(number)) {
+      return false;
+    } else if (number.length == 10) {
+      return true;
+    }
+  };
+
+  const { update } = useContext(AuthContext);
+  const { logout } = useContext(AuthContext);
+
+  const updateProfile = (key, value) => {
+    setProfile(prevState => ({
+      ...prevState,
+      [key]: value,
+    }));
+  };
+
+  // FONTS
+
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  const getIsFormValid = () => {
+    return (
+      !validateName(profile.firstName) &&
+      !validateName(profile.lastName) &&
+      validateEmail(profile.email) &&
+      validateNumber(profile.phoneNumber)
+    );
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setProfile(prevState => ({
+        ...prevState,
+        ["image"]: result.assets[0].uri,
+      }));
+    }
+  };
+
+  const removeImage = () => {
+    setProfile(prevState => ({
+      ...prevState,
+      ["image"]: "",
+    }));
+  };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <TouchableOpacity style={styles.selectButton} onPress={pickImage}>
-        <Text style={styles.buttonText}>Pick an image</Text>
-      </TouchableOpacity>
-      <View style={styles.imageContainer}>
-      {image && <Image source={{ uri: image.uri }} style={{ width: 300, height: 300 }} />}
-      
-      <TouchableOpacity style={styles.uploadButton} onPress={uploadImage}>
-        <Text style={styles.buttonText}>Upload image</Text>
-      </TouchableOpacity>
-        
-        
-      </View>
-    </SafeAreaView>
-  )
-}
-
-export default Profile
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      onLayout={onLayoutRootView}
+    >
+      <ScrollView style={styles.viewScroll}>
+        <Text style={styles.headertext}>Personal information</Text>
+        <Text style={styles.text}>Avatar</Text>
+        <View style={styles.avatarContainer}>
+          {profile.image ? (
+            <Image source={{ uri: profile.image }} style={styles.avatarImage} />
+          ) : (
+            <View style={styles.avatarEmpty}>
+              <Text style={styles.avatarEmptyText}>
+                {profile.firstName && Array.from(profile.firstName)[0]}
+                {profile.lastName && Array.from(profile.lastName)[0]}
+              </Text>
+            </View>
+          )}
+          <View style={styles.avatarButtons}>
+            <Pressable
+              style={styles.changeBtn}
+              title="Pick an image from camera roll"
+              onPress={pickImage}
+            >
+              <Text style={styles.saveBtnText}>Change</Text>
+            </Pressable>
+            <Pressable
+              style={styles.removeBtn}
+              title="Pick an image from camera roll"
+              onPress={removeImage}
+            >
+              <Text style={styles.discardBtnText}>Remove</Text>
+            </Pressable>
+          </View>
+        </View>
+        <Text
+          style={[
+            styles.text,
+            !validateName(profile.firstName) ? "" : styles.error,
+          ]}
+        >
+          First Name
+        </Text>
+        <TextInput
+          style={styles.inputBox}
+          value={profile.firstName}
+          onChangeText={newValue => updateProfile("firstName", newValue)}
+          placeholder={"First Name"}
+        />
+        <Text
+          style={[
+            styles.text,
+            !validateName(profile.lastName) ? "" : styles.error,
+          ]}
+        >
+          Last Name
+        </Text>
+        <TextInput
+          style={styles.inputBox}
+          value={profile.lastName}
+          onChangeText={newValue => updateProfile("lastName", newValue)}
+          placeholder={"Last Name"}
+        />
+        <Text
+          style={[
+            styles.text,
+            validateEmail(profile.email) ? "" : styles.error,
+          ]}
+        >
+          Email
+        </Text>
+        <TextInput
+          style={styles.inputBox}
+          value={profile.email}
+          keyboardType="email-address"
+          onChangeText={newValue => updateProfile("email", newValue)}
+          placeholder={"Email"}
+        />
+        <Text
+          style={[
+            styles.text,
+            validateNumber(profile.phoneNumber) ? "" : styles.error,
+          ]}
+        >
+          Phone number (10 digit)
+        </Text>
+        <TextInput
+          style={styles.inputBox}
+          value={profile.phoneNumber}
+          keyboardType="phone-pad"
+          onChangeText={newValue => updateProfile("phoneNumber", newValue)}
+          placeholder={"Phone number"}
+        />
+        <Pressable style={styles.btn} onPress={() => logout()}>
+          <Text style={styles.btntext}>Log out</Text>
+        </Pressable>
+        <View style={styles.buttons}>
+          <Pressable
+            style={[styles.saveBtn, getIsFormValid() ? "" : styles.btnDisabled]}
+            onPress={() => update(profile)}
+            disabled={!getIsFormValid()}
+          >
+            <Text style={styles.saveBtnText}>Save changes</Text>
+          </Pressable>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      backgroundColor: '#000',
-      justifyContent:'center'
-    },
-    selectButton: {
-      borderRadius: 5,
-      width: 150,
-      height: 50,
-      backgroundColor: 'blue',
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    uploadButton: {
-      borderRadius: 5,
-      width: 150,
-      height: 50,
-      backgroundColor: 'red',
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: 20
-    },
-    buttonText: {
-      color: 'white',
-      fontSize: 18,
-      fontWeight: 'bold'
-    },
-    imageContainer: {
-      marginTop: 30,
-      marginBottom: 50,
-      alignItems: 'center'
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  header: {
+    padding: 12,
+    flexDirection: "row",
+    justifyContent: "center",
+    backgroundColor: "#dee3e9",
+  },
+  logo: {
+    height: 50,
+    width: 150,
+    resizeMode: "contain",
+  },
+  viewScroll: {
+    flex: 1,
+    padding: 10,
+  },
+  headertext: {
+    fontSize: 22,
+    paddingBottom: 10,
+    fontWeight: "extrabold",
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 5,
+    fontWeight: "medium",
+  },
+  inputBox: {
+    alignSelf: "stretch",
+    marginBottom: 10,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderRadius: 9,
+    borderColor: "#dfdfe5",
+  },
+  btn: {
+    backgroundColor: "#f4ce14",
+    borderRadius: 9,
+    alignSelf: "stretch",
+    marginVertical: 18,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#cc9a22",
+  },
+  btnDisabled: {
+    backgroundColor: "#98b3aa",
+  },
+  buttons: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 60,
+  },
+  saveBtn: {
+    flex: 1,
+    backgroundColor: "#495e57",
+    borderRadius: 9,
+    alignSelf: "stretch",
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#3f554d",
+  },
+  saveBtnText: {
+    fontSize: 18,
+    color: "#FFFFFF",
+    alignSelf: "center",
+    fontWeight: "bold",
+  },
+  discardBtn: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 9,
+    alignSelf: "stretch",
+    marginRight: 18,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#83918c",
+  },
+  discardBtnText: {
+    fontSize: 18,
+    color: "#3e524b",
+    alignSelf: "center",
+    fontWeight: "bold",
+  },
+  btntext: {
+    fontSize: 22,
+    color: "#3e524b",
+    fontWeight: "bold",
+    alignSelf: "center",
+  },
+  section: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  paragraph: {
+    fontSize: 15,
+  },
+  checkbox: {
+    margin: 8,
+  },
+  error: {
+    color: "#d14747",
+    fontWeight: "bold",
+  },
+  avatarContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  avatarImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+  },
+  avatarEmpty: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    backgroundColor: "#0b9a6a",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  avatarEmptyText: {
+    fontSize: 32,
+    color: "#FFFFFF",
+    fontWeight: "bold",
+  },
+  avatarButtons: {
+    flexDirection: "row",
+  },
+  changeBtn: {
+    backgroundColor: "#495e57",
+    borderRadius: 9,
+    marginHorizontal: 18,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#3f554d",
+  },
+  removeBtn: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 9,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#83918c",
+  },
+});
 
-    
-  });
+export default Profile;
